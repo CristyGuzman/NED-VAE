03a96cb8a3931e22555ce348ffb7751a30edd7a5
CristyGuzman <csolis@student.ethz.ch>
2022-07-27 10:12:30 +0200

    fix file path

--- compute_mc_scores.sh
+++ compute_mc_scores.sh
@@ -9,7 +9,7 @@ for MODEL_I in FactorVAE_1658867433_10 FactorVAE_1658878251_20 FactorVAE_1658878
 do
     for MODEL_J in FactorVAE_1658867433_10 FactorVAE_1658878251_20 FactorVAE_1658878306_1 FactorVAE_1658878329_10 FactorVAE_1658878329_10 FactorVAE_1658878488_20 FactorVAE_1658878564_40 FactorVAE_1658879092_1 FactorVAE_1658879962_10 FactorVAE_1658880548_20 FactorVAE_1658880677_40 FactorVAE_1658880769_1 FactorVAE_1658880919_10 FactorVAE_1658880930_20 FactorVAE_1658881391_40
     do
-        sbatch -p gpu --cpus-per-task 2 --mem-per-cpu 8G --gres=gpu:1 --wrap "/home/csolis/nedvae_repo/NED-VAE/model_centrality.sh $MODEL_I $MODEL_J $1 $2"
+        sbatch -p gpu --cpus-per-task 2 --mem-per-cpu 8G --gres=gpu:1 --wrap "/home/csolis/forked_repo_nedvae/NED-VAE/model_centrality.sh $MODEL_I $MODEL_J $1 $2"
    
     done
 done
