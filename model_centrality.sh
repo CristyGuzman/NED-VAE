10f45ef948050722cf39299cb6fa3c027aa50d96
CristyGuzman <csolis@student.ethz.ch>
2022-07-27 08:48:06 +0200

    fix factor file directory for dci

--- model_centrality.sh
+++ model_centrality.sh
@@ -39,8 +39,8 @@ NODES_FILE="/home/csolis/forked_repo_nedvae/generated_nodes/WS_graph_testing2___
 python /home/csolis/forked_repo_nedvae/NED-VAE/generate_embeddings.py --model_centrality 1 --fix_factor_values 0 --generate_graphs 0 --graphs_file $GRAPHS_FILE --nodes_file $NODES_FILE --model_file $MODEL_I_DIR --num_factors $DIMS
 
 
-EMBEDDINGS="/home/csolis/forked_repo_nedvae/embeddings/${MODEL_I}_WS_graph_testing2___generated_graphs_z.npy" 
-FACTOR_EMBEDDINGS="/home/csolis/forked_repo_nedvae/NED-VAE/models/FactorVAE/trained_models/${MODEL_J}_testing2___z.npy"
+EMBEDDINGS="/home/csolis/forked_repo_nedvae/embeddings/${MODEL_I}_z.npy" 
+FACTOR_EMBEDDINGS="/home/csolis/forked_repo_nedvae/NED-VAE/models/FactorVAE/trained_models/${MODEL_J}_WS_graph_testing2_testing2___z.npy"
 
 python /home/csolis/forked_repo_nedvae/NED-VAE/DCI_metric.py --embeddings_file $EMBEDDINGS --factors_file $FACTOR_EMBEDDINGS --save_file $SAVE_MC_DIR
 
