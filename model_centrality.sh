ee885ab7eafc168fbf5487f1d0fb8c0802023c97
CristyGuzman <csolis@student.ethz.ch>
2022-07-27 08:25:07 +0200

    fix fila directory

--- model_centrality.sh
+++ model_centrality.sh
@@ -22,13 +22,13 @@ if [ $METRIC == "FactorVAE" ] ; then
 GRAPHS_FILE="/home/csolis/forked_repo_nedvae/generated_graphs/WS_graph_testing2_fixed_${DIMS}_generated_graphs.npy" 
 NODES_FILE="/home/csolis/forked_repo_nedvae/generated_nodes/WS_graph_testing2_fixed_${DIMS}_generated_nodes.npy" 
 
-python /home/csolis/nedvae_repo/NED-VAE/generate_embeddings.py --model_centrality 1 --fix_factor_values 0 --generate_graphs 0 --graphs_file $GRAPHS_FILE --nodes_file $NODES_FILE --model_file $MODEL_I_DIR --num_factors $DIMS
+python /home/csolis/forked_repo_nedvae/NED-VAE/generate_embeddings.py --model_centrality 1 --fix_factor_values 0 --generate_graphs 0 --graphs_file $GRAPHS_FILE --nodes_file $NODES_FILE --model_file $MODEL_I_DIR --num_factors $DIMS
 
 
 EMBEDDINGS="/home/csolis/forked_repo_nedvae/embeddings/${MODEL_I}_WS_graph_testing2_fixed_${DIMS}_generated_graphs_z.npy" 
 LABELS="/home/csolis/forked_repo_nedvae/labels/${MODEL_I}_WS_graph_testing2_fixed_${DIMS}_generated_graphs_labels.npy" 
 
-python /home/csolis/nedvae_repo/NED-VAE/Factor_metric.py --embeddings_files $EMBEDDINGS --labels_files $LABELS --save_file $SAVE_MC_DIR
+python /home/csolis/forked_repo_nedvae/NED-VAE/Factor_metric.py --embeddings_files $EMBEDDINGS --labels_files $LABELS --save_file $SAVE_MC_DIR
 
 fi
 
@@ -36,12 +36,12 @@ if [ $METRIC == "DCI" ] ; then
 GRAPHS_FILE="/home/csolis/forked_repo_nedvae/generated_graphs/WS_graph_testing2___generated_graphs.npy" 
 NODES_FILE="/home/csolis/forked_repo_nedvae/generated_nodes/WS_graph_testing2___generated_nodes.npy" 
 
-python /home/csolis/nedvae_repo/NED-VAE/generate_embeddings.py --model_centrality 1 --fix_factor_values 0 --generate_graphs 0 --graphs_file $GRAPHS_FILE --nodes_file $NODES_FILE --model_file $MODEL_I_DIR --num_factors $DIMS
+python /home/csolis/forked_repo_nedvae/NED-VAE/generate_embeddings.py --model_centrality 1 --fix_factor_values 0 --generate_graphs 0 --graphs_file $GRAPHS_FILE --nodes_file $NODES_FILE --model_file $MODEL_I_DIR --num_factors $DIMS
 
 
 EMBEDDINGS="/home/csolis/forked_repo_nedvae/embeddings/${MODEL_I}_WS_graph_testing2___generated_graphs_z.npy" 
-FACTOR_EMBEDDINGS="/home/csolis/nedvae_repo/NED-VAE/models/FactorVAE/trained_models/${MODEL_J}_testing2___z.npy"
+FACTOR_EMBEDDINGS="/home/csolis/forked_repo_nedvae/NED-VAE/models/FactorVAE/trained_models/${MODEL_J}_testing2___z.npy"
 
-python /home/csolis/nedvae_repo/NED-VAE/DCI_metric.py --embeddings_file $EMBEDDINGS --factors_file $FACTOR_EMBEDDINGS --save_file $SAVE_MC_DIR
+python /home/csolis/forked_repo_nedvae/NED-VAE/DCI_metric.py --embeddings_file $EMBEDDINGS --factors_file $FACTOR_EMBEDDINGS --save_file $SAVE_MC_DIR
 
 fi
