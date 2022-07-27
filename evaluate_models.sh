b5d287c64a771702b297e4980ee713621665a437
CristyGuzman <csolis@student.ethz.ch>
2022-07-27 08:29:42 +0200

    create script for evaluating models supervised

---
+++ evaluate_models.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+## For generating embeddings from original test graphs, run evaluate.py with 
+# model_centrality=0
+# fix_factor_values=0 
+# generate_graphs=0 
+# model_file=PATH/TO/MODEL.ckpt
+
+set -e
+while [ $# -gt 0 ] 
+do
+    echo $1
+    sbatch -p gpu --cpus-per-task 2 --mem-per-cpu 8G --gres=gpu:1 --nodelist bs-gpu08,bs-gpu07 --wrap "python /home/csolis/nedvae_repo/NED-VAE/evaluate.py --vae_type FactorVAE --model_centrality 0 --fix_factor_values 0 --generate_graphs 0 --model_file $1"
+    shift
+done
